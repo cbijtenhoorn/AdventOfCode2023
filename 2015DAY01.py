97e9d933435383a3ca0b1f3ce040df78d786f36d
cbijtenhoorn <c.bijtenhoorn@hotmail.com>
2023-11-29 22:13:12 +0100

    Practice mode

---
+++ 2015DAY01.py
@@ -0,0 +1,18 @@
+with open('2015DAY01INPUT.txt') as input:
+    for line in input:
+        instructions = [*line]
+
+counter = 0
+# index = 0 #PART TWO
+for step in instructions:
+    # index += 1 #PART TWO
+    if step == '(':
+        counter += 1
+    if step == ')':
+        counter -= 1
+    # if counter < 0: #PART TWO
+    #     break
+    
+
+print(counter)
+# print(index) #PART TWO
